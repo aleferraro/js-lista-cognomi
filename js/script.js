e6d10a97f60083ec977a5946e5f0fc083538759c
Alessandro Ferraro <ale_ferraro@libero.it>
2020-11-15 16:55:34 +0100

    Code Refactoring
    introduce comments adjustments

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-// definisco la lista dei cognomi
+/*
+Lista Cognomi
+Chiedere all'utente il cognome
+inserirlo in un array con altri cognomi: 'Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'
+stampa la lista ordinata alfabeticamente
+scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+*/
 
+// definisco la lista dei cognomi
 var surnameList = ['Rossi', 'Verdi', 'Bianchi', 'Bianco'];
 
 //raccolgo l'input dell'utente e lo aggiungo alla lista dei cognomi
 var userSurname = prompt('Inserisci il tuo cognome');
-
 surnameList.push(userSurname);
 
 console.log(surnameList);
@@ -13,6 +19,7 @@ console.log(surnameList);
 surnameList.sort();
 
 console.log(surnameList);
+
 // vado a cercare l'elmento ol nell'html
 var olEl = document.getElementById('surnameList');
 
@@ -20,11 +27,13 @@ var olEl = document.getElementById('surnameList');
 var userPosition = 0;
 
 for (var i = 0; i < surnameList.length; i++) {
+  //stampo a video la lista dei cognomi
   olEl.innerHTML += '<li>' + surnameList[i] + '</li>';
 
   if (surnameList[i] == userSurname){
-    userPosition = i;
+    userPosition = i + 1;
   }
 }
 
-document.getElementById('userPosition').innerHTML += (userPosition+1);
+//stampo a video la posizione del cognome dell'utente
+document.getElementById('userPosition').innerHTML += userPosition;
